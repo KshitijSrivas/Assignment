83ac770d2b6734ed508a2e5b99b32d604c6fd201
Kshitij Srivastava <errror0007@gmail.com>
2020-09-03 00:51:46 +0530

    Adding test project

---
+++ AssignmentTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssignmentTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssignmentTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0ca30f2-aa84-4855-a567-f38d5d373b15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssignmentTest/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssignmentTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Product productA = new Product("A", 50);
+            Product productB = new Product("B", 30);
+            Product productC = new Product("C", 20);
+            Product productD = new Product("D", 15);
+
+            Promotion promotionA = new Promotion();
+            promotionA.Products.Add(productA);
+            promotionA.Products.Add(productA);
+            promotionA.Products.Add(productA);
+            promotionA.PromotionalCost = 130;
+
+            Promotion promotionB = new Promotion();
+            promotionA.Products.Add(productB);
+            promotionA.Products.Add(productB);
+            
+            promotionB.PromotionalCost = 45;
+
+            Promotion compositePromotion = new Promotion();
+            compositePromotion.Products.Add(productC);
+            compositePromotion.Products.Add(productD);
+            compositePromotion.PromotionalCost = 30;
+
+            Order orderA = new Order(productA, 1);
+            Order orderB = new Order(productB, 1);
+            Order orderC = new Order(productC, 1);
+
+            Cart cartService = new Cart();
+            cartService.AddOrder(orderA);
+            cartService.AddOrder(orderB);
+            cartService.AddOrder(orderC);
+
+            cartService.AddPromotion(promotionA);
+            cartService.AddPromotion(promotionB);
+            cartService.AddPromotion(compositePromotion);
+
+            cartService.CalculateTotalPrice();
+        }
+    }
+}
